f7eb161502601543ea6d1ff2d744ad7bc8ef62d9
dhxogud <hyong0117@naver.com>
2025-07-15 10:38:49 +0900

    Initial commit

---
+++ Source/P20250715_Task/Network/LoginClientSubsystem.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "LoginClientSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class P20250715_TASK_API ULoginClientSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	ULoginClientSubsystem();
+};
---
+++ Source/P20250715_Task/P20250715_Task.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "P20250715_Task.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, P20250715_Task, "P20250715_Task" );
